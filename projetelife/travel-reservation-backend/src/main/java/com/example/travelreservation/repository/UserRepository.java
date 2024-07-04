e34b10d2fd196af9b57fe09509fa264b64f9345c
mizouni <5752@holbertonstudents.com>
2024-07-04 14:11:16 +0100

    new

--- projetelife/travel-reservation-backend/src/main/java/com/example/travelreservation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travelreservation.repository;
-
-import com.example.travelreservation.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
